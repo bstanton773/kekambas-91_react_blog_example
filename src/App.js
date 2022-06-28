f80a102c91b7e13d88170aed9254d45367f8caf3
Brian <bstanton773@gmail.com>
2022-06-28 11:23:46 -0500

    Add register view and set up BrowserRouter with react-router-dom

--- src/App.js
+++ src/App.js
@@ -1,13 +1,18 @@
 import React from 'react';
+import { Routes, Route } from 'react-router-dom';
 import Nav from './components/Nav';
 import Home from './views/Home';
+import Register from './views/Register';
 
 export default function App() {
     return (
         <>
             <Nav />
             <div className='container'>
-                <Home />
+                <Routes>
+                    <Route path='/' element={<Home />} />
+                    <Route path='/register' element={<Register />} />
+                </Routes>
             </div>
         </>
     )
--- src/components/Nav.js
+++ src/components/Nav.js
@@ -1,18 +1,19 @@
-import React from 'react'
+import React from 'react';
+import { Link } from 'react-router-dom';
 
 export default function Nav(props) {
     return (
         <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
             <div className="container-fluid">
-                <a className="navbar-brand" href="/">Navbar</a>
+                <Link className="navbar-brand" to="/">My Blog</Link>
                 <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
                 <span className="navbar-toggler-icon"></span>
                 </button>
                 <div className="collapse navbar-collapse" id="navbarNavAltMarkup">
                 <div className="navbar-nav">
-                    <a className="nav-link" href="/">Home</a>
-                    <a className="nav-link" href="/">Register</a>
-                    <a className="nav-link" href="/">Login</a>
+                    <Link className="nav-link" to="/">Home</Link>
+                    <Link className="nav-link" to="/register">Register</Link>
+                    <Link className="nav-link" to="/login">Login</Link>
                 </div>
                 </div>
             </div>
